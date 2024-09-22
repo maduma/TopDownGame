4fbc8f077659f48de9b6111484dcd8ce881587cb
maduma <maduma@pt.lu>
2024-09-22 22:11:35 +0200

    check godot version in edit script

--- script/edit.bat
+++ script/edit.bat
@@ -10,6 +10,15 @@ if ERRORLEVEL 1 (
     EXIT /B
 )
 
+:: Check Godot version
+set version=4
+godot --version | findstr /B %version%
+if ERRORLEVEL 1 (
+    ECHO godot version "%version%" needed.
+    PAUSE
+    EXIT /B
+)
+
 :: Launch the editor
 CD Tuto
 godot --rendering-driver opengl3 -e
